1107b0600d74b6bb5a1d041a6e29da92e302b15b
Hiroshi Saito <hiroshi3110@gmail.com>
2013-11-01 13:26:15 +0900

    Add guid if none using link content.

--- feedmodifier.rb
+++ feedmodifier.rb
@@ -2,6 +2,13 @@ require 'nokogiri'
 
 def feed(input)
   doc = Nokogiri::XML(input){|x| x.noblanks }
+  doc.xpath("//item").each do |item_node|
+    if item_node.xpath("guid").empty?
+      guid_node = Nokogiri::XML::Node.new("guid", doc)
+      guid_node.content = item_node.at_xpath("comments").content
+      item_node.add_child(guid_node)
+    end
+  end
   doc.to_xml(:indent => 2)
 end
 
